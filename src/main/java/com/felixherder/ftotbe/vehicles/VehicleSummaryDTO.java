3397630659812fa99341738f17f5f697dda924af
felixHerder <felixbaciu@gmail.com>
2026-04-23 10:22:26 +0300

    fix openapi spec response type

--- src/main/java/com/felixherder/ftotbe/vehicles/VehicleSummaryDTO.java
+++ src/main/java/com/felixherder/ftotbe/vehicles/VehicleSummaryDTO.java
@@ -1,15 +1,21 @@
 package com.felixherder.ftotbe.vehicles;
 
 import com.felixherder.ftotbe.vehicles.enums.TransmissionType;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.Year;
 
 
 public record VehicleSummaryDTO(
+        @NotNull
         String uuid,
+        @NotNull
         String model,
+        @NotNull
         String make,
+        @NotNull
         Year year,
+        @NotNull
         TransmissionType transmissionType,
         String imageUrl
 ) {
