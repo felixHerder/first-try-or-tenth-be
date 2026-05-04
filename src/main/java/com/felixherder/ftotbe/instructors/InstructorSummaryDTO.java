b7635968e3a27e5e966bbbf2de37ff66353e3fa7
felixHerder <felixbaciu@gmail.com>
2026-05-04 14:37:21 +0300

    extend sql init script

--- src/main/java/com/felixherder/ftotbe/instructors/InstructorSummaryDTO.java
+++ src/main/java/com/felixherder/ftotbe/instructors/InstructorSummaryDTO.java
@@ -1,8 +1,12 @@
 package com.felixherder.ftotbe.instructors;
 
 import com.felixherder.ftotbe.profiles.ProfileDTO;
+import jakarta.validation.constraints.NotNull;
+
 public record InstructorSummaryDTO(
+        @NotNull
         String uuid,
+        @NotNull
         ProfileDTO profile
 ) {
 }
--- src/main/java/com/felixherder/ftotbe/trainees/TraineeSummaryDTO.java
+++ src/main/java/com/felixherder/ftotbe/trainees/TraineeSummaryDTO.java
@@ -1,9 +1,12 @@
 package com.felixherder.ftotbe.trainees;
 
 import com.felixherder.ftotbe.profiles.ProfileDTO;
+import jakarta.validation.constraints.NotNull;
 
 public record TraineeSummaryDTO(
+        @NotNull
         String uuid,
+        @NotNull
         ProfileDTO profile
 ) {
 }
